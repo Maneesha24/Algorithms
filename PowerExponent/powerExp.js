3fb71992baf2d7d8af193d884530aee0ab782a95
Maneesha <venigallamaneesha24@gmail.com>
2018-08-15 23:13:11 +0530

    power exponent

---
+++ PowerExponent/powerExp.js
@@ -0,0 +1,6 @@
+function powerExp(a,b){
+	const c = Math.pow(a,b);
+	return c;
+}
+
+module.exports = powerExp;
---
+++ PowerExponent/powerExp.test.js
@@ -0,0 +1,13 @@
+const powerExp = require('./powerExp.js');
+
+test('To find the power exponent',()=>{
+	expect(powerExp(2,5)).toBe(32);
+})
+
+test('To find the power exponent',()=>{
+	expect(powerExp(10,2)).toBe(100);
+})
+
+test('To find the power exponent',()=>{
+	expect(powerExp(25,3)).toBe(15625);
+})
