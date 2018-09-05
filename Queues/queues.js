00b8c8a734ef678b3609bfe9a875b1ec024ad9f3
Maneesha <venigallamaneesha24@gmail.com>
2018-09-05 05:25:01 -1100

    queues committed

---
+++ Queues/queues.js
@@ -0,0 +1,40 @@
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue{
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  enQueue(val){
+    var newNode = new Node(val);
+    if(!this.head){
+      this.head = newNode;
+      this.tail = newNode;
+    }else{
+      this.tail.next = newNode;
+     this.tail= newNode;
+    }
+    return this.length++;
+  }
+
+  deQueue(){
+    if(!this.head){
+      return;
+    }
+    if(!this.head.next){
+      this.head = null;
+      this.tail = null;
+      return;
+    }
+    this.head = this.head.next;
+    this.length--;
+    
+  }
+}
